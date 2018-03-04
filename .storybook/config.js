b24b8b2fe47b26cac1d1ad69e20c4b56276f62b0
Oleg Pimenov <pimenov.o@gmail.com>
2018-03-04 13:30:05 +0300

    remove unused files and added stories

---
+++ .storybook/config.js
@@ -0,0 +1,18 @@
+
+import { configure, addDecorator } from '@storybook/angular';
+import { setOptions } from '@storybook/addon-options';
+import { withKnobs } from '@storybook/addon-knobs/angular';
+
+setOptions({
+  name: 'Angular Material'
+});
+
+addDecorator(withKnobs);
+
+// automatically import all files ending in *.stories.ts
+const req = require.context('../stories', true, /.stories.ts$/);
+function loadStories() {
+  req.keys().forEach((filename) => req(filename));
+}
+
+configure(loadStories, module);
