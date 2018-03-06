0ea5d9b749075347dcafdcce64cf79502d98a078
Oleg Pimenov <pimenov.o@gmail.com>
2018-03-06 19:54:15 +0300

    updated button story

--- stories/button.stories.ts
+++ stories/button.stories.ts
@@ -4,17 +4,20 @@ import { McButtonModule } from '@ptsecurity/mosaic';
 
 
 storiesOf('Button', module)
-    .add('Primary', () => ({
+    .add('buttons', () => ({
         template: `
             <div>
-                <button mc-button color="primary" [disabled]="disabled">{{label}}</button>
+                <button mc-button color="primary" [disabled]="disabled">{{label_Primary}}</button>
                 <br><br>
                 <button mc-button color="second">second</button>
+                <br><br>
+                <button class="mc-progress" mc-button color="second">{{label_Progress}}</button>
             </div>
         `,
         props: {
             disabled: boolean('disabled', false),
-            label: text('label', 'primary'),
+            label_Primary: text('label_Primary', 'primary'),
+            label_Progress: text('label_Progress', 'progress'),
         },
         moduleMetadata: {
             imports: [McButtonModule]
