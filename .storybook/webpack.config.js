d9d4a6b4c5f3378fdc44df593f58b736b5b49a42
Oleg Pimenov <opimenov@ptsecurity.com>
2018-05-29 22:41:59 +0300

    updated yarn lock

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
-
 
 module.exports = (baseConfig) => {
 
@@ -11,13 +9,8 @@ module.exports = (baseConfig) => {
             {
                 from: 'node_modules/@ptsecurity/mosaic/prebuilt-themes/default-theme.css',
                 to: 'assets/css/default-theme.css'
-            },
-        ]),
-        new ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)(@angular|esm5|fesm5)/,
-            path.resolve(__dirname, 'node_modules')
-        )
+            }
+        ])
     );
 
     return baseConfig;
