727fd4da1978016a1fba66333908213782fb6db0
Oleg Pimenov <opimenov@ptsecurity.com>
2018-05-30 15:01:56 +0300

    added callback for deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,11 @@ const path = require('path');
 const { publish } = require('gh-pages');
 
 const dir = path.resolve(path.join(__dirname, '../', 'storybook-static'));
+
 publish(dir, {
-  user: {name: 'PTSecurity'},
-  message: '(deploy): CI'
-});
+        user: {
+            name: 'PTSecurity'
+        },
+        message: '(deploy): CI'
+    },
+    console.error.bind(console));
