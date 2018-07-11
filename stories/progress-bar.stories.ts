0cc245486644b50f98f6492acefc5ae84ca13a83
evilfant <ifruty@gmail.com>
2018-07-11 22:07:53 +0700

    feat(progress-bar): added example (#2)

---
+++ stories/progress-bar.stories.ts
@@ -0,0 +1,36 @@
+import { storiesOf } from '@storybook/angular';
+import { number, select } from '@storybook/addon-knobs';
+import { McProgressBarModule } from '@ptsecurity/mosaic';
+
+const modes = {
+    determinate: 'determinate',
+    indeterminate: 'indeterminate'
+}
+
+const colors = {
+    primary: 'primary',
+    second: 'second',
+    warn: 'warn'
+};
+
+storiesOf('Progress bar', module)
+    .add('Simple progress bar', () => ({
+        template: `
+            <div>
+                <h1>Progress bar</h1>
+                <mc-progress-bar [mode]="mode"
+                                 [value]="value"
+                                 [color]="color">
+                </mc-progress-bar>
+                <br><br>
+            </div>
+        `,
+        props: {
+            mode: select('mode', modes, modes.determinate),
+            value: number('value', 30, { min: 0, max: 100, step: 5 }),
+            color: select('color', colors, colors.primary)
+        },
+        moduleMetadata: {
+            imports: [McProgressBarModule]
+        }
+    }));
