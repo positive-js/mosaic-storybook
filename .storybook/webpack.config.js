a8af5542b2ba03b054c88ac745785828f0399a33
Oleg Pimenov <pimenov.o@gmail.com>
2018-07-24 22:51:11 +0300

    feat(addon): added storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
@@ -14,6 +16,20 @@ module.exports = (baseConfig) => {
                 to: 'assets/css/default-theme.css'
             },
         ])
+    );
+
+    baseConfig.module.rules.push(
+        {
+            test: [/\.stories\.tsx?$/, /index\.ts$/],
+            loaders: [
+                {
+                    loader: require.resolve('@storybook/addon-storysource/loader'),
+                    options: { parser: 'typescript' }
+                }
+            ],
+            include: [path.resolve(__dirname, '../stories')],
+            enforce: 'pre'
+        }
     );
 
     return baseConfig;
