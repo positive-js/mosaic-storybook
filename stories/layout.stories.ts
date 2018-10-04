1ecba362c74a58a3f68bb8e26ff1a54a12b761e6
Andrey Rodkin <andrey.rodkin@gmail.com>
2018-10-04 15:47:13 +0300

    feat(layout): Initial documentation for flex layout with simple example (#9)
    * feat(layout): included layout styles
    
    * feat(layout): simple layout example

--- stories/layout.stories.ts
+++ stories/layout.stories.ts
@@ -1,15 +1,30 @@
 import { storiesOf } from '@storybook/angular';
-import { boolean, text } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 
 
 storiesOf('Layout|Layout Flex', module)
-    .add('Layout Flex', () => ({
+    .add('Simple Layout', () => ({
         template: `
-            <div>
+            <style>
+                .block {
+                    border: 1px solid black;
+                    height: 100px;
+                    padding: 10px;
+                    margin: 10px;
+                }
+            </style>
+            
+            <div>                  
+                <h1 class="mc-h1">Single direction (use knobs on the bottom to change direction)</h1>
                 
+                <div [class]=layoutDirection>
+                    <div class="flex block">first</div>
+                    <div class="flex block">second</div>
+                    <div class="flex block">third</div>
+                </div>                
             </div>
         `,
         props: {
-
+            layoutDirection: select('Direction for child blocks', { 'layout-column': 'layout-column', 'layout-row': 'layout-row'  })
         }
     }));
