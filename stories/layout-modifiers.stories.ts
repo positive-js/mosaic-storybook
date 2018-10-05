2c616233ef99d0dae2c3543d070307d29707f49a
Andrey Rodkin <arodkin@ptsecurity.com>
2018-10-05 10:09:08 +0300

    feat(layout): flex-initial example

--- stories/layout-modifiers.stories.ts
+++ stories/layout-modifiers.stories.ts
@@ -26,4 +26,29 @@ storiesOf('Layout|Layout Flex\/Behaviour modifiers', module)
         props: {
             parentWidth: number('Parent container width, %', 1000, { range: true, min: 20, max: 1000, step: 1}),
         }
+    }))
+    .add('flex-initial', () => ({
+        template: `
+            <style>
+                .block {
+                    border: 1px solid black;
+                    padding: 10px;
+                    margin: 10px;
+                }
+            </style>
+            
+            <h1 class="mc-h1">flex-initial</h1>
+            <p class="mc-text-left">It shrinks to its minimum size to fit the container, but does not grow to absorb any extra free space in the flex container</p>
+                
+            <div [style.width.px]="parentWidth">
+                <div class="layout-row block">
+                    <div class="flex block">flex</div>
+                    <div class="flex-initial block">flex-initial</div>
+                    <div class="flex-initial block">flex-initial</div>
+                </div>
+            </div>
+        `,
+        props: {
+            parentWidth: number('Parent container width, %', 1000, { range: true, min: 20, max: 1000, step: 1}),
+        }
     }));
