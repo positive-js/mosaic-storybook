9f79f066dbe1024673318f48ba1ba21cf508b78e
Andrey Rodkin <andrey.rodkin@gmail.com>
2018-10-05 17:50:18 +0300

    feat(layout): Offset example (#14)
    * feat(layout): order example
    
    * feat(layout): move aux function into separate file
    
    * feat(layout): move aux func into separate file
    
    * feat(layout): offset example

---
+++ stories/layout-offsets.stories.ts
@@ -0,0 +1,37 @@
+import { storiesOf } from '@storybook/angular';
+import { select } from '@storybook/addon-knobs';
+import * as utils from '../utils';
+
+storiesOf('Layout|Layout Flex', module)
+    .add('Offsets (base)', () => ({
+        template: `
+            <style>
+                .block {
+                    border: 1px solid black;
+                    padding: 10px;
+                }
+            </style>
+            
+            <div>                  
+                <h1 class="mc-h1">Offset</h1>
+                
+                <div class="layout-row block">
+                    <div class="{{size}} {{offset}} block">{{size}} {{offset}}</div>
+                    <div class="{{size}} {{offset}} block">{{size}} {{offset}}</div>
+                    <div class="{{size}} {{offset}} block">{{size}} {{offset}}</div>
+                </div>                
+            </div>
+        `,
+        props: {
+            size: select(
+                'Size',
+                utils.toObj([ 0, 5, 10, 15, 20, 25, 30, 33, 35, 40, 45, 50, 60, 65, 66, 70, 75, 80, 85, 90, 95, 100 ], 'flex-'),
+                'flex-20'
+            ),
+            offset: select(
+                'Offset',
+                utils.toObj([ 0, 5, 10, 15, 20, 25, 30, 33, 35, 40, 45, 50, 60, 65, 66, 70, 75, 80, 85, 90, 95, 100 ], 'flex-offset-'),
+                'flex-offset-0'
+            )
+        }
+    }));
