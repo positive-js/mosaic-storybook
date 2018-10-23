8d9491defe2a03b1974d7789130553b0cfc3fb4f
RistiCore <risticore@mail.ee>
2018-10-23 16:14:40 +0300

    feat: Story splitted to several ones

--- stories/timepicker.stories.ts
+++ stories/timepicker.stories.ts
@@ -22,16 +22,15 @@ function myDateKnob(name, defaultValue) {
 }
 
 storiesOf('Form Controls|Timepicker', module)
-    .add('Any time allowed', () => ({
-            template: `
+    .add('Basic display customization', () => ({
+        template: `
+        <p>Component may be disabled, customize time format and inner icon visibility.</p>
         <div style="width: 150px">
             <mc-form-field >
                 <i *ngIf="isIconVisible" mcPrefix mc-icon="mc-clock_16"></i>
                 <input mcTimepicker
                        [(ngModel)]="value"
                        [disabled]="isDisabled"
-                       [min-time]="minTime"
-                       [max-time]="maxTime"
                        [time-format]="timeFormat">
             </mc-form-field>            
         </div>
@@ -39,22 +38,51 @@ storiesOf('Form Controls|Timepicker', module)
             <span *ngSwitchCase="true">Result: {{value.toLocaleTimeString()}}</span>
             <span *ngSwitchCase="false">Null result</span>
         </section>`,
-            props: {
-                isIconVisible: boolean('Is timepicker with icon', true),
-                isDisabled: boolean('disabled', false),
-                value: myDateKnob('Time value', newDefaultTime),
-                timeFormat: select('Time format', ['HH:mm', 'HH:mm:ss'], 'HH:mm'),
-                minTime: text('Minimal time allowed (blank if no)', ''),
-                maxTime: text('Maximal time allowed (blank if no)', '')
-            },
-            moduleMetadata: {
-                imports: [
-                    FormsModule,
-                    McTimepickerModule,
-                    McInputModule,
-                    McFormFieldModule,
-                    McIconModule
-                ]
-            }
-        })
-    );
+        props: {
+            value: myDateKnob('Time value', newDefaultTime),
+            isIconVisible: boolean('Show icon', true),
+            isDisabled: boolean('Disable', false),
+            timeFormat: select('Time format', ['HH:mm', 'HH:mm:ss'], 'HH:mm')
+        },
+        moduleMetadata: {
+            imports: [
+                FormsModule,
+                McTimepickerModule,
+                McInputModule,
+                McFormFieldModule,
+                McIconModule
+            ]
+        }
+    }))
+    .add('Min/max time limitations', () => ({
+        template: `
+        <p>Minimum and maximum time may be custimized using min-time and max-time params.</p>
+        <div style="width: 150px">
+            <mc-form-field >
+                <i *ngIf="isIconVisible" mcPrefix mc-icon="mc-clock_16"></i>
+                <input mcTimepicker
+                       [(ngModel)]="value"
+                       [min-time]="minTime"
+                       [max-time]="maxTime">
+            </mc-form-field>            
+        </div>
+        <section [ngSwitch]="value !== null">
+            <span *ngSwitchCase="true">Result: {{value.toLocaleTimeString()}}</span>
+            <span *ngSwitchCase="false">Null result</span>
+        </section>`,
+        props: {
+            value: myDateKnob('Time value', newDefaultTime),
+            isIconVisible: boolean('Is timepicker with icon', true),
+            minTime: text('Minimal time allowed (blank if no)', '10:00:00'),
+            maxTime: text('Maximal time allowed (blank if no)', '13:00:00')
+        },
+        moduleMetadata: {
+            imports: [
+                FormsModule,
+                McTimepickerModule,
+                McInputModule,
+                McFormFieldModule,
+                McIconModule
+            ]
+        }
+    }));
