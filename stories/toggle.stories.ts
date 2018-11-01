5129f2d70f9e1d11ff7a94a3d058ed0619d728a1
roll314 <kornyushin.petr@gmail.com>
2018-11-01 15:52:44 +0300

    feat(toggle): storybook (#24)

---
+++ stories/toggle.stories.ts
@@ -0,0 +1,55 @@
+import { storiesOf } from '@storybook/angular';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { McToggleModule } from '@ptsecurity/mosaic';
+
+
+storiesOf('Form Controls|Toggle', module)
+    .addDecorator(withKnobs)
+    .add('Toggle', () => ({
+        template: `
+            <style>
+                .toggle-container {
+                    margin-bottom: 16px;
+                }            
+            </style>
+            <div class="toggle-container">
+                <mc-toggle class="mc-toggle_small" [checked]="true" [disabled]="disabled">
+                    Small toggle with label on right
+                </mc-toggle>
+            </div>
+            <div class="toggle-container">
+                <mc-toggle class="mc-toggle_small cdk-keyboard-focused" [checked]="true" [disabled]="disabled">
+                    Focused small toggle
+                </mc-toggle>
+            </div>           
+            
+            <div class="toggle-container">
+                <mc-toggle [checked]="true" [disabled]="disabled">
+                    Default toggle with label on right
+                </mc-toggle>
+            </div>
+            <div class="toggle-container">
+                <mc-toggle class="cdk-keyboard-focused" [checked]="true" [disabled]="disabled">
+                    Focused default toggle
+                </mc-toggle>
+            </div>
+
+            <div class="toggle-container">
+                <mc-toggle labelPosition="left" [checked]="true" [disabled]="disabled">
+                    Default toggle with label on left
+                </mc-toggle>
+            </div>
+            <div class="toggle-container">
+                <mc-toggle class="mc-toggle_small" labelPosition="left" [checked]="true" [disabled]="disabled">
+                    Small toggle with label on left
+                </mc-toggle>
+            </div>
+        `,
+        props: {
+            disabled: boolean('disabled', false)
+        },
+        moduleMetadata: {
+            imports: [McToggleModule]
+        }
+    }));
+
