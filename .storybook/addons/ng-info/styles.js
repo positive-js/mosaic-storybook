b15df2a7524394abbbd2500d1c20dd1ff0378202
Oleg Pimenov <pimenov.o@gmail.com>
2019-01-12 13:50:00 +0300

    feat: added docs for button

--- .storybook/addons/ng-info/styles.js
+++ .storybook/addons/ng-info/styles.js
@@ -12,5 +12,21 @@ h3[id|="mrkd"] { font-size: 1.25em; }
 
 #mrkd-root > .with-ng-info-example {
     border: 2px dashed #ddd;
+    padding: 8px;
+}
+
+pre {
+    padding: 8px;
+    background: #eee;
+    border-radius: 8px;
+}
+#mrkd-root > table th,
+#mrkd-root > table td,
+#mrkd-root > table {
+    border-collapse: collapse;
+    font-family: sans-serif;
+    border: 1px solid #eee;
+    text-align: left;
+    padding: 12px;
 }
 `;
