3c720d3122abf39b875d63cc2adf27fff0e2d239
Dinara Nikolaeva <dnikolaeva@ptsecurity.com>
2019-02-04 15:17:56 +0300

    feat(sidepanel): Add story for sidepanel component

---
+++ stories/sidepanel.stories.ts
@@ -0,0 +1,26 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { McButtonModule, McIconModule, McSidepanelModule, McSidepanelPosition } from '@ptsecurity/mosaic';
+import { storiesOf } from '@storybook/angular';
+import { boolean, select } from '@storybook/addon-knobs';
+
+import { withAnyInfo } from '../.storybook/addons/ng-info';
+
+import { ExampleSidepanelComponent, SidepanelDemoComponent } from './sidepanel/sidepanel.component';
+
+
+storiesOf('Popups & Modals|Sidepanel', module)
+    .add('Sidepanel', () => ({
+        component: SidepanelDemoComponent,
+        moduleMetadata: {
+            imports: [BrowserAnimationsModule, McSidepanelModule, McButtonModule, McIconModule],
+            declarations: [ExampleSidepanelComponent],
+            entryComponents: [ExampleSidepanelComponent]
+        },
+        props: {
+            position: select('position', [
+                McSidepanelPosition.Left, McSidepanelPosition.Right, McSidepanelPosition.Top, McSidepanelPosition.Bottom
+            ], McSidepanelPosition.Left),
+            hasBackdrop: boolean('hasBackdrop', true),
+            disableClose: boolean('disableClose', false)
+        }
+    }));
---
+++ stories/sidepanel/sidepanel.component.ts
@@ -0,0 +1,75 @@
+import { Component, Inject, Input, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MC_SIDEPANEL_DATA, McSidepanelPosition, McSidepanelService } from '@ptsecurity/mosaic';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: './sidepanel.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class SidepanelDemoComponent {
+    @Input() position: McSidepanelPosition;
+    @Input() hasBackdrop: boolean;
+    @Input() disableClose: boolean;
+
+    @ViewChild(TemplateRef) template: TemplateRef<any>;
+
+    constructor(private sidepanelService: McSidepanelService) {}
+
+    openComponentSidepanel() {
+        this.sidepanelService.open(ExampleSidepanelComponent, {
+            position: this.position,
+            hasBackdrop: this.hasBackdrop,
+            disableClose: this.disableClose,
+            data: {
+                openComponentSidepanel: () => { this.openComponentSidepanel(); }
+            }
+        });
+    }
+
+    openTemplateSidepanel() {
+        this.sidepanelService.open(this.template, {
+            position: this.position,
+            hasBackdrop: this.hasBackdrop,
+            disableClose: this.disableClose
+        });
+    }
+}
+
+@Component({
+    selector: 'example-sidepanel',
+    template: `
+    <mc-sidepanel-header [closeable]="false">
+        Sidepanel Component Header
+    </mc-sidepanel-header>
+    <mc-sidepanel-body class="layout-padding">
+        <div class="mc-subheading">Sidepanel Component Body</div>
+
+        <div *ngFor="let item of array; index as i">
+            {{ i + 1 }}
+        </div>
+    </mc-sidepanel-body>
+    <mc-sidepanel-footer>
+        <mc-sidepanel-actions align="right">
+            <button mc-button color="primary" (click)="openComponentSidepanel()">
+                <span>Open another sidepanel</span>
+            </button>
+
+            <button mc-button color="second" mc-sidepanel-close>
+                <span>Close</span>
+            </button>
+        </mc-sidepanel-actions>
+    </mc-sidepanel-footer>`,
+    host: {
+        class: 'layout-column flex'
+    }
+})
+export class ExampleSidepanelComponent {
+    openComponentSidepanel: () => void;
+
+    array = new Array(60);
+
+    constructor(@Inject(MC_SIDEPANEL_DATA) public data: any) {
+        this.openComponentSidepanel = data.openComponentSidepanel;
+    }
+}
