361185a9fff6e03e71351979786480c3806e863c
Peter Kornuishin <pkornyushin@ptsecurity.com>
2019-04-08 14:37:24 +0300

    chore: yarn.lock update

---
+++ stories/button-toggle.stories.ts
@@ -0,0 +1,52 @@
+import {storiesOf} from '@storybook/angular';
+import {boolean, text} from '@storybook/addon-knobs';
+
+import {McButtonToggleModule} from '@ptsecurity/mosaic';
+
+import {withAnyInfo} from '../.storybook/addons/ng-info';
+
+import * as markdown from './button.stories.md';
+
+
+storiesOf('Buttons|Button Toggle', module)
+    .addDecorator(withAnyInfo)
+
+    .addParameters({
+        anyinfo: {
+            markdown
+        }
+    })
+    .add('Button Toggle', () => ({
+        template: `
+            <mc-button-toggle-group [(ngModel)]="modelResult">
+                <mc-button-toggle value="1">
+                    default 1
+                </mc-button-toggle>
+                <mc-button-toggle value="2" disabled>
+                    default 2
+                </mc-button-toggle>
+                <mc-button-toggle value="3">
+                    default 3
+                </mc-button-toggle>
+                <mc-button-toggle value="4">
+                    <i mc-icon="mc-angle-down-L_16"></i>
+                </mc-button-toggle>
+            </mc-button-toggle-group>
+            <div class="example-selected-value">Model result: {{modelResult}}</div>
+            
+            <br>
+            <br>
+            
+            <mc-button-toggle value="123" #standAloneToggle="mcButtonToggle">
+                default
+            </mc-button-toggle>
+            <div class="example-selected-value">Selected value: {{standAloneToggle.checked}}</div>
+        `,
+        props: {
+            disabled: boolean('disabled', false),
+            modelResult: text('Model result', '1')
+        },
+        moduleMetadata: {
+            imports: [McButtonToggleModule]
+        }
+    }));
