1888a89df866abe086bfe578c246658c4929c6de
arvasilev <arvasilev@ptsecurity.com>
2019-04-12 17:47:23 +0700

    feat(icons): UID-20 - added mosaic-icons doc

--- stories/icons.stories.ts
+++ stories/icons.stories.ts
@@ -1,10 +1,17 @@
 import { storiesOf } from '@storybook/angular';
-import { boolean, text } from '@storybook/addon-knobs';
+import { withAnyInfo } from '../.storybook/addons/ng-info';
+import * as markdown from './icons.stories.md';
 
 const iconsHTML = require('../node_modules/@ptsecurity/mosaic-icons/dist/html/mc-icons.html');
 
 
 storiesOf('Indicators|Icons', module)
+    .addDecorator(withAnyInfo)
+    .addParameters({
+        anyinfo: {
+            markdown
+        }
+    })
     .add('Icons List', () => ({
         template: iconsHTML
     }));
